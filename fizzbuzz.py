38e8eafbc12f287df35ec7907d0ca83eb3d0bcd2
Katie Cassady <katie@cassadyweb.com>
2023-10-29 15:20:40 -0400

    resubmit2

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
 # list numbers. If divisible by 3 print fizz. If divisible by 5 print buzz. If divisible by 3 and 5 print fizzbuzz
 for i in range(1,101):
-    if (i % 3 == 0) and (i % 5 != 0):
-        print("Fizz")
-    if (i % 5 == 0) and (i % 3 != 0):
-        print("Buzz")
-    if (i % 5 == 0) and (i % 3 == 0):
+    if (i % 3 == 0) and (i % 5 == 0):
         print("FizzBuzz")
+    elif (i % 5 == 0):
+        print("Buzz")
+    elif (i % 3 == 0):
+        print("Fizz")
     else:
         print(i)
